1087c43c287573c0d58a221a812121da8278c2cf
정민 <36464297+Teiming@users.noreply.github.com>
2018-12-10 17:43:05 +0900

    로그 생성기

--- Log Writer.sh
+++ Log Writer.sh
@@ -1,1 +1,8 @@
-#Affinity
+#Designer
+diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ja.lproj/ > ~/Affinity-in-Korean/Log/en-ja.log
+diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ko.lproj/ > ~/Affinity-in-Korean/Log/en-ko.log
+#Photo
+#Designer(grep)
+diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ja.lproj/ | grep -E "<string>|.nib|.strings" > ~/Affinity-in-Korean/Log/en-ja.log
+diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ko.lproj/ | grep -E "<string>|.nib|.strings" > ~/Affinity-in-Korean/Log/en-ko.log
+#Photo(grep)
