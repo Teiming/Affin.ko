3b1ac95c21c020bad1f18defef346cff07366a74
정민 <36464297+Teiming@users.noreply.github.com>
2018-12-10 21:29:02 +0900

    로그 생성

--- logWriter.sh
+++ logWriter.sh
@@ -1,8 +1,12 @@
 #Designer
-diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ja.lproj/ > ~/Affinity-in-Korean/Log/en-ja.log
-diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ko.lproj/ > ~/Affinity-in-Korean/Log/en-ko.log
+diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ja.lproj/ > ~/Affinity-in-Korean/Log/afdesigner.en-ja.log
+diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ko.lproj/ > ~/Affinity-in-Korean/Log/afdesigner.en-ko.log
 #Photo
+diff -s /Applications/Affinity\ Photo.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Photo.app/Contents/Resources/ja.lproj/ > ~/Affinity-in-Korean/Log/afphoto.en-ja.log
+diff -s /Applications/Affinity\ Photo.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Photo.app/Contents/Resources/ko.lproj/ > ~/Affinity-in-Korean/Log/afphoto.en-ko.log
 #Designer(grep)
-diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ja.lproj/ | grep -E "<string>|.nib|.strings" > ~/Affinity-in-Korean/Log/grep.en-ja.log
-diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ko.lproj/ | grep -E "<string>|.nib|.strings" > ~/Affinity-in-Korean/Log/grep.en-ko.log
+diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ja.lproj/ | grep -E "<string>|.nib|.strings" > ~/Affinity-in-Korean/Log/afdesigner.grep.en-ja.log
+diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Designer.app/Contents/Resources/ko.lproj/ | grep -E "<string>|.nib|.strings" > ~/Affinity-in-Korean/Log/afdesigner.grep.en-ko.log
 #Photo(grep)
+diff -s /Applications/Affinity\ Photo.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Photo.app/Contents/Resources/ja.lproj/ | grep -E "<string>|.nib|.strings" > ~/Affinity-in-Korean/Log/afphoto.grep.en-ja.log
+diff -s /Applications/Affinity\ Photo.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Photo.app/Contents/Resources/ko.lproj/ | grep -E "<string>|.nib|.strings" > ~/Affinity-in-Korean/Log/afphoto.grep.en-ko.log
