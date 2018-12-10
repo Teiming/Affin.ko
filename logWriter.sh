9cf15ebe275634a844697aa4f294c8b79e3b2b18
정민 <36464297+Teiming@users.noreply.github.com>
2018-12-10 22:03:36 +0900

    사전 비교 추가

--- logWriter.sh
+++ logWriter.sh
@@ -10,3 +10,5 @@ diff -s /Applications/Affinity\ Designer.app/Contents/Resources/en.lproj/ /Appli
 #Photo(grep)
 diff -s /Applications/Affinity\ Photo.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Photo.app/Contents/Resources/ja.lproj/ | grep -E "<string>|.nib|.strings" > ~/Affinity-in-Korean/Log/afphoto.grep.en-ja.log
 diff -s /Applications/Affinity\ Photo.app/Contents/Resources/en.lproj/ /Applications/Affinity\ Photo.app/Contents/Resources/ko.lproj/ | grep -E "<string>|.nib|.strings" > ~/Affinity-in-Korean/Log/afphoto.grep.en-ko.log
+# 사전 동기화
+diff ~/Affinity-in-Korean/designerdic ~/Affinity-in-Korean/photodic > ~/Affinity-in-Korean/Log/syncDictionary.log
